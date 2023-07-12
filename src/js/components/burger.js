0395d2df49d6d57fa2c1f8f16ccf31f405f82637
Maxim <maxim.trz@gmail.com>
2023-07-12 15:53:01 +0500

    fix menu

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -4,15 +4,20 @@
 const burgerBtn = document.querySelector(".burger-btn");
 const headerContainer = document.querySelector(".header__container");
 const mainMenu = document.querySelector(".main-menu");
+const logo = document.querySelector(".header__logo");
+const container = document.querySelector(".header__container");
 
 const openBurger = function () {
   burgerBtn.classList.add("--open");
   mainMenu.classList.add("--open");
+  container.classList.add("--open");
+  logo.style.opacity = 0;
 };
 
 const closeBurger = function () {
   burgerBtn.classList.remove("--open");
   mainMenu.classList.remove("--open");
+  logo.style.opacity = 1;
 };
 
 burgerBtn.addEventListener("click", (e) => {
