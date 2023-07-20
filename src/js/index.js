96c5dbefa25b948cd89013c985ced18bf259582c
MaximTrz <maxim.trz@gmail.com>
2023-07-20 23:21:00 +0500

    fix truncate

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
 //= libs.js
 
-window.addEventListener("load", function () {
+document.addEventListener("DOMContentLoaded", function () {
   //= components/swiper.js
+});
+
+window.addEventListener("load", function () {
   //= components/burger.js
   //= components/ontop.js
   //= components/news_paragraph.js
