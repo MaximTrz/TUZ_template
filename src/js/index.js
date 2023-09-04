f6c2ee73e7a38a6f3e718f0e573e33acff68b3d6
MaximTrz <maxim.trz@gmail.com>
2023-09-04 09:13:05 +0500

    04_09

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,9 @@
-//= libs.js
-
 document.addEventListener("DOMContentLoaded", function () {
   //= components/swiper.js
 });
 
 window.addEventListener("load", function () {
+  //= components/news_paragraph.js
   //= components/burger.js
   //= components/ontop.js
-  //= components/news_paragraph.js
 });
